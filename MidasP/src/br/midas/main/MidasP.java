9535df797f7d004d95c63f906c0616223136eaa1
NU92585 <NU92585@LABCB-1WQY442.nr.ad.newellco.com>
2017-09-11 12:11:38 -0300

    Commit inicial

--- MidasP/src/br/midas/main/MidasP.java
+++ MidasP/src/br/midas/main/MidasP.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package midasp;
+package br.midas.main;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
